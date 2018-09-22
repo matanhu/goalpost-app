2998cedf3cb6b41d681df97b62bd29df580bbe52
MatanHu <MatanHuja@gmail.com>
2018-09-22 15:24:44 +0300

    Building CreateGoalVC

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,37 @@
+//
+//  GcreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by MatanHuja on 22/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
