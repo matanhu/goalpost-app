4b5b2a9deae20aa75c4ab803612e45660cce01dd
MatanHu <MatanHuja@gmail.com>
2018-09-22 11:52:18 +0300

    Displaying Static GoalCells in UITableView, Creating GoalType Enum

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by MatanHuja on 22/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case lognTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
