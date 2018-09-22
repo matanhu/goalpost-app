ce0a1aeb41ed099889668a8b2373670066e21a4a
MatanHu <MatanHuja@gmail.com>
2018-09-22 11:22:05 +0300

    Building GoalCell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by MatanHuja on 22/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
